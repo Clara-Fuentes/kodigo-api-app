69a8c468ba93e255c3193ba7cbf05728d610aab2
Alejo7d1 <alejo7d1@gmail.com>
2025-09-15 18:49:39 -0600

    feature: add components Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from "./components/NavBar"
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar/>
       <h1>Kodigo-appi-app</h1>
+      <Footer/>
     </>
   )
 }
